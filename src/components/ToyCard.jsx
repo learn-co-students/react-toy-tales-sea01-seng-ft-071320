31b5395f6324c040967197412dd2f6967c384b2f
Alec DuBois <after.alec@gmail.com>
2020-09-13 13:41:57 -0700

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ToyCard extends Component {
+  handleLike = () => {
+    return this.props.likeToy(this.props.toy.id, this.props.toy.likes + 1);
+  };
+
+  handleDelete = () => this.props.deleteToy(this.props.toy.id);
 
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.toy.name}</h2>
+        <img
+          src={this.props.toy.image}
+          alt={this.props.toy.name}
+          className="toy-avatar"
+        />
+        <p>{this.props.toy.likes} Likes </p>
+        <button onClick={this.handleLike} className="like-btn">
+          Like {"<3"}
+        </button>
+        <button onClick={this.handleDelete} className="del-btn">
+          Donate to GoodWill
+        </button>
       </div>
     );
   }
-
 }
 
 export default ToyCard;
